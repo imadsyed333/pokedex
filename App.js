e7b75d545c42932205928aaadbcd64392cb9a2b6
Imad <creeperone23@gmail.com>
2022-08-23 15:48:20 -0400

    Added title, changed styling

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import { Platform, SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet, Text, View, StatusBar, Image } from 'react-native';
 import Pokedex from './components/Pokedex';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.title}>Pokedex</Text>
+        <Image style={styles.logo} source={{uri: "https://www.freepnglogos.com/uploads/pokemon-symbol-logo-png-31.png"}}/>
+      </View>
       <Pokedex/>
     </SafeAreaView>
   );
@@ -17,4 +21,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
+  title: {
+    fontSize: 40,
+    fontWeight:"500",
+    flexDirection: 'row',
+    alignItems:'center',
+    alignSelf: 'center'
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    padding: 10,
+  }
 });

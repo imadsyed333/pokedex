10c6ef11ab3fb688e83ef48b60a5ad0e98eee6a6
Imad <creeperone23@gmail.com>
2022-08-23 00:38:41 -0400

    Created main layout

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
+import Pokedex from './components/Pokedex';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Pokedex/>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +15,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
 });

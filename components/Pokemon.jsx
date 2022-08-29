bc1a99dcd2d695195cc87dec9ff1764969b5c5f6
Imad <creeperone23@gmail.com>
2022-08-29 14:29:47 -0400

    Removed function to fetch sprites, now using props

--- components/Pokemon.jsx
+++ components/Pokemon.jsx
@@ -1,30 +1,25 @@
 import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
 
-export default function Pokemon(props) {
-  const [spriteLink, setSpriteLink] = useState("");
-  useEffect(() => {
-    getSprite();
-  }, []);
-  const getSprite = () => {
-    const url = "https://pokeapi.co/api/v2/pokemon/" + props.name;
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        setSpriteLink(json.sprites.front_default);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("Cannot fetch image");
-      });
-  };
+const Pokemon = React.memo(function Pokemon(props) {
   return (
-    <TouchableOpacity style={styles.container}>
-      <Image style={styles.sprite} source={{ uri: spriteLink }}></Image>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => {
+        props.navigation.navigate("Pokemon", {
+          name: props.name,
+          sprite: props.sprite,
+          art: props.art,
+        });
+      }}
+    >
+      <Image style={styles.sprite} source={{ uri: props.sprite }}></Image>
       <Text style={styles.text}>{props.name}</Text>
     </TouchableOpacity>
   );
-}
+});
+
+export { Pokemon };
 
 const styles = StyleSheet.create({
   container: {

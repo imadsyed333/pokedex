af0e51d504e11c52f0553d3eed875a6f3d2a3106
Imad <creeperone23@gmail.com>
2022-08-29 14:29:08 -0400

    Moved App,js content to HomeScreen.jsx

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, SafeAreaView, Text, Image, View } from "react-native";
+import Pokedex from "../components/Pokedex";
+
+export default function HomeScreen(props) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.title}>Pokedex</Text>
+        <Image
+          style={styles.logo}
+          source={{
+            uri: "https://www.freepnglogos.com/uploads/pokemon-symbol-logo-png-31.png",
+          }}
+        />
+      </View>
+      <Pokedex navigation={props.navigation} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EFF8E2",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    padding: 10,
+  },
+});

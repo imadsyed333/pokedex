761b47b6661521aadbecd56aeb51677717167e03
Imad <creeperone23@gmail.com>
2022-09-05 00:03:42 -0400

    Removed useless styles and imports

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Platform, SafeAreaView, StyleSheet, Text, View, StatusBar, Image } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import PokemonScreen from './screens/PokemonScreen';
 
@@ -16,25 +15,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EFF8E2',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-  },
-  title: {
-    fontSize: 40,
-    fontWeight:"bold",
-    flexDirection: 'row',
-    alignItems:'center',
-    alignSelf: 'center'
-  },
-  logo: {
-    width: 50,
-    height: 50,
-    padding: 10,
-  }
-});

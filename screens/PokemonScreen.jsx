2d072b560f16af4b405835930d9aafc6894e67c4
Imad <creeperone23@gmail.com>
2022-09-05 00:01:52 -0400

    Style changes

--- screens/PokemonScreen.jsx
+++ screens/PokemonScreen.jsx
@@ -1,16 +1,37 @@
 import React from "react";
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Dimensions,
+} from "react-native";
 
 export default function PokemonScreen({ route }) {
+  const Types = ({ pokemon }) => {
+    const typeString = "";
+    const types = pokemon.types;
+    for (let i = 0; i < types; i++) {
+      console.log(types[i].type.name);
+      typeString.concat(types[i].type.name, ", ");
+    }
+    return <Text style={styles.text}>Types: {typeString}</Text>;
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.card}>
         <Image style={styles.sprite} source={{ uri: route.params.art }} />
-        <Text>{route.params.name}</Text>
+        <Text style={[styles.text, styles.title]}>{route.params.name}</Text>
+        <Types pokemon={route.params.pokemon} />
+        <Text style={styles.text}></Text>
       </View>
     </SafeAreaView>
   );
 }
+
+const spriteDim = Dimensions.get("screen").width - 50;
 
 const styles = StyleSheet.create({
   container: {
@@ -23,9 +44,18 @@ const styles = StyleSheet.create({
   card: {
     backgroundColor: "#EE6352",
     borderRadius: 10,
+    padding: 10,
   },
   sprite: {
-    height: 400,
-    width: 400,
+    height: spriteDim,
+    width: spriteDim,
+  },
+  text: {
+    fontSize: 20,
+    color: "#EFF8E2",
+    padding: 10,
+  },
+  title: {
+    fontWeight: "bold",
   },
 });
